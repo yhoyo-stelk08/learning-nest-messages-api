f5004d40ebd3706c88574241617f6f427274b65b
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-08-15 18:21:45 +0800

    feat: using ValidationPipe in main for validating user input

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { MessagesModule } from './messages/messages.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(MessagesModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(3001);
 }
 bootstrap();
