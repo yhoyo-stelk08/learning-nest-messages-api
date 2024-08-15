a70093c2c8170362598df3ea87969ebb014e9e5e
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-08-15 19:20:14 +0800

    feat: create new service called MessagesService

---
+++ src/messages/messages.service.ts
@@ -0,0 +1,21 @@
+import { MessagesRepository } from './messages.repository';
+
+export class MessagesService {
+  messagesRepo: MessagesRepository = new MessagesRepository();
+
+  constructor() {
+    this.messagesRepo = new MessagesRepository();
+  }
+
+  findaAll() {
+    return this.messagesRepo.findAll();
+  }
+
+  findOne(id: string) {
+    return this.messagesRepo.findOne(id);
+  }
+
+  create(content: string) {
+    return this.messagesRepo.create(content);
+  }
+}
