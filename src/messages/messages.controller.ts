5244aa576b75debb197912b64b3e374233f28ca3
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-08-15 18:28:11 +0800

    feat: create new DTO files called CreateMessageDto and use it in MessageController.

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
 
 @Controller('messages')
-export class MessagesController {}
+export class MessagesController {
+  @Get()
+  listMessages() {
+    return 'List all messages';
+  }
+
+  @Post()
+  createMessage(@Body() body: CreateMessageDto) {
+    console.log(body);
+    return 'Create a message';
+  }
+
+  @Get(':id')
+  getMessage(@Param('id') id: string) {
+    console.log(id);
+    return 'Get a message';
+  }
+}
